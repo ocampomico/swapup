6cc17522d574a7b4088eec0248cda5375ea57e60
cnazir14 <cnazir14@apu.edu>
2018-01-19 21:00:30 -0800

    Added SearchScreen's Layout & Navigation

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
+import Expo, { Notifications } from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import SearchScreen from './screens/SearchScreen';
+import SearchResultsScreen from './screens/SearchResultsScreen';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const { data: { text }, origin } = notification;
+
+      if (origin === 'received' && text) {
+        Alert.alert(
+          'New Push Notification',
+          text, 
+          [{ text: 'ok.' }]
+        );
+      }
+    });
+  }
+
   render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: SearchScreen },
+          home: { screen: HomeScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        }, {
+          tabBarPosition: 'bottom', //location of the tab bar
+          //swipeEnabled: false,
+          tabBarOptions: {
+            labelStyle: { fontSize: 12 }
+          }
+        })
+      }
+    }, {
+      navigationOptions: {
+        tabBar: { visible: false }
+      },
+      lazy: true
+    });
     return (
-      <View style={styles.container}>
-        <Text>Welcome to SwapUp!Caroline is great ? ?????</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
