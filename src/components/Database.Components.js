b4856eaee6feebfa5a7571a2d9b8dadda740406f
cnazir14 <cnazir14@apu.edu>
2018-04-06 12:00:34 -0700

    Added City Search
    Checkin DG

---
+++ src/components/Database.Components.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+        AppRegistry, FlatLiist,
+        StyleSheet, Text, View, Image, Alert, Platform,
+        TouchableHighLight,
+        RefreshControl, TextInput
+} from 'reract-native';
+import firebase from 'react-native-firebase';
+
+const iosConfig = {
+
+};
+
+const androidconfig = {
+
+};
